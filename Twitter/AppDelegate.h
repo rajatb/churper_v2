787dda070628054b9c4485ef09c2a2a88a1b5eb8
Rajat Bhargava <rajat.bhargava@gmail.com>
2014-06-19 13:15:19 -0700

    Initial Commit

---
+++ Twitter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Twitter
+//
+//  Created by Bhargava, Rajat on 6/19/14.
+//  Copyright (c) 2014 rnb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Twitter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Twitter
+//
+//  Created by Bhargava, Rajat on 6/19/14.
+//  Copyright (c) 2014 rnb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
